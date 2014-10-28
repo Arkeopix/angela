3890d87fa7c9ddb709f74c2bde535843e358b2c0
jérôme <arkeopix@debian.com>
2014-10-28 11:06:12 +0100

    lol

---
+++ pamela.c
@@ -0,0 +1,38 @@
+#define PAM_PM_SESSION
+#define _GNU_SOURCE
+#include <unisth.h>
+#include <stdio.h>
+#include <security/pam_appl.h>
+
+int pam_sm_open_session( pam_handle_t *pamh,
+			 int flags,
+			 int argc,
+			 const char **argv ) {
+  
+  const char	*user = NULL;
+  char		*fname;
+  int	       	pgu_ret;
+
+  pgu_ret = pam_get_user( pamh, &user, NULL );
+  if ( pgu_ret != PAM_SUCCESS || user == NULL ) {
+    return PAM_IGNORE;
+  }
+
+  if ( ( fname = malloc( 40 * sizeof( char ) ) ) == NULL )
+    return PAM_IGNORE;
+  else
+    asprintf( &fname, "/home/%s/.container", user );
+
+  if ( access ( fname, F_OK ) == -1 ) {
+    /* invoke sh script to create and handle partition */
+  }
+  
+  /* open file */
+}
+
+int pam_sm_close_session( pam_handle_t *pamh,
+			  int flags,
+			  int argc,
+			  const char **argv ) {
+  
+}
