8d4eab098a0d433bffb685b2be5d0d27059a2452
jérôme <arkeopix@debian.com>
2014-10-28 16:33:30 +0100

    create_container_done

--- create_container.sh
+++ create_container.sh
@@ -1,10 +1,24 @@
-#!/usr/bin/sh
+#!/bin/sh
 
-if [ $# -eq 0 ]
+if [ $# -ne 3 ]
 then
-    echo "$0 username"
+    echo "$0 username container_name part_size"
     exit 1
 fi
 
-dd if=/dev/zero of=/home/$1/container bs=1M count=512
-cryptsetup -y luksFormat /home/$1/container
+if [ ! -d "/home/$1/container" ]
+then
+    echo "Creating /home/$1/container/$2"
+    mkdir /home/$1/container
+fi
+
+echo "creating partition in /home/$1/container/$2"
+dd if=/dev/zero of=/home/$1/container/$2 bs=1M count=$3
+echo "formating with luksFormat"
+cryptsetup -y luksFormat /home/$1/container/$2
+echo "opening container"
+cryptsetup luksOpen /home/$1/container/$2 $3_handle
+echo "applying ext4"
+mkfs.ext4 -j /dev/mapper/$3_handle
+echo "closing containner"
+cryptsetup luksClose $3_handle
