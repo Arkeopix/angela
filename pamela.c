24bc0106958d206ff6a00b496983db7e094fc513
jérôme <arkeopix@debian.com>
2014-10-29 10:10:35 +0100

    poil

--- pamela.c
+++ pamela.c
@@ -1,8 +1,15 @@
 #define PAM_PM_SESSION
 #define _GNU_SOURCE
-#include <unisth.h>
 #include <stdio.h>
+#include <string.h>
+#include <unisth.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include <security/pam_appl.h>
+
+static int fork_exec_shell_script( char *user, char *container ) {
+
+}
 
 int pam_sm_open_session( pam_handle_t *pamh,
 			 int flags,
@@ -10,8 +17,11 @@ int pam_sm_open_session( pam_handle_t *pamh,
 			 const char **argv ) {
   
   const char	*user = NULL;
+  char		container[26];
   char		*fname;
   int	       	pgu_ret;
+  DIR		*dp;
+  struct dirent	*ep;
 
   pgu_ret = pam_get_user( pamh, &user, NULL );
   if ( pgu_ret != PAM_SUCCESS || user == NULL ) {
@@ -20,15 +30,22 @@ int pam_sm_open_session( pam_handle_t *pamh,
 
   if ( ( fname = malloc( 40 * sizeof( char ) ) ) == NULL ) {
     return PAM_IGNORE;
-  } else {
-    asprintf( &fname, "/home/%s/container/test", user );
   }
 
-  if ( access ( fname, F_OK ) != -1 ) {
-    /* Open and Mount container */
+  asprintf( &fname, "/home/%s/container/", user );
+  if ( ( dp = opendir( fname ) ) != NULL ) {
+    /* Open and Mount containers */
+    while ( ep = readdir( dp ) ){
+      memset( &container, 0, 26 );
+      container = ep->d_name;
+      if ( ( fork_exec_shell_script( user, container ) ) != 0 ) {
+	return PAM_IGNORE;
+      }
+    }
   } else {
     return PAM_IGNORE;
   }
+  return PAM_SUCCESS;
 }
 
 int pam_sm_close_session( pam_handle_t *pamh,
