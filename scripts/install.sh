8efbc70189823f78133d7ab35883ff5958848cc6
jérôme <arkeopix@debian.com>
2014-10-29 13:25:25 +0100

    insatll script done

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-echo "session		optional	pamela.so" >> /etc/pam.d/login
 echo "making shared library"
 make
 echo "copying lib to /lib/security"
-cp pamela.so /lib/security
+cp src/pamela.so /lib/security
+
+string="session optional pamela.so"
+
+if ! grep -xqx "$string" /etc/pam.d/login
+then
+    echo $string >> /etc/pam.d/login
+fi
