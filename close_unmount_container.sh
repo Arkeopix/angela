a6afd63f064cf35abfde9af4f1a5351534dd2f8b
jérôme <arkeopix@debian.com>
2014-10-29 09:36:58 +0100

    coucou

--- close_unmount_container.sh
+++ close_unmount_container.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if [ $# -ne 2 ]
+then
+    echo "$0 username container_name"
+    exit 0
+fi
 umount /home/$1/container/mounted/$2
 cryptsetup luksClose $2Handle
--- open_mount_container.sh
+++ open_mount_container.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-cryptsetup luksOpen /home/$1/container/$2 $2Handle<<EOF
-lol
-EOF
+if [ $# -ne 2 ]
+then
+    echo "$0 username container_name"
+    exit 1
+fi
+
 if [ ! -d "/home/$1/container/mounted" ]
 then
     mkdir /home/$1/container/mounted
+fi
+
+cryptsetup luksOpen /home/$1/container/$2 $2Handle<<EOF
+lol
+EOF
+if [ ! -d "/home/$1/container/mounted/$2" ]
+then
+    mkdir /home/$1/container/mounted/$2
 fi
 mount /dev/mapper/$2Handle /home/$1/container/mounted/$2
