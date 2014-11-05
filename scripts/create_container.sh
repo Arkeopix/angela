3b6cd050878b8e28bd78d7fb0df8f87f79157d55
Arkeopix <mendiej750@gmail.com>
2014-11-05 12:52:10 +0100

    update create container

--- scripts/create_container.sh
+++ scripts/create_container.sh
@@ -22,3 +22,34 @@ echo "applying ext4"
 mkfs.ext4 -j /dev/mapper/$2Handle
 echo "closing containner"
 cryptsetup luksClose $2Handle
+
+echo "tell me, what was the password did you enter ?"
+read passwd
+
+if [ ! -e /etc/ctpasswd ]
+then
+    echo "touching /etc/ctpasswd"
+    touch /etc/ctpasswd
+fi
+
+enc_pass=$( openssl passwd -1 $passwd )
+echo "$1:$enc_pass" >> /etc/ctpasswd
+
+echo "i'm going to mail it to $1"
+
+echo "
+hi $1 !
+
+this is an automatic mail from your administrator.
+You receive it because you were deemed worthy of handling your own 
+secure container.
+
+here is your container name: $2
+and here is the password which you'll need to do stuff to it: $passwd
+
+go and update your ~/.container file
+
+Have fun =)
+" | mail -s "New container" $1
+
+echo "all done. see you soon =)"
